7a8f07a3725c7b97fdc0aaf5a75f753ff570c458
Jasper van Putten <jasper@splendo.com>
2014-11-17 12:34:40 +0100

    use unsigned int for log and move hex string to util

--- LeBlue/src/main/java/houtbecke/rs/le/LeUtil.java
+++ LeBlue/src/main/java/houtbecke/rs/le/LeUtil.java
@@ -10,7 +10,7 @@ public class LeUtil {
     public static String bytesToString(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte b: bytes) {
-            builder.append(b).append(",");
+            builder.append(b & 0xFF).append(",");
         }
         String bytesString = builder.toString();
         if (bytesString.length() > 0)
@@ -26,6 +26,32 @@ public class LeUtil {
             bytes[k] = Byte.valueOf(tokenizer.nextToken());
         return bytes;
     }
+
+    public static byte[] hexStringToBytes(String s) {
+        int len = s.length();
+        byte[] data = new byte[len/2];
+
+        for(int i = 0; i < len; i+=2){
+            data[i/2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i+1), 16));
+        }
+
+        return data;
+    }
+
+    final protected static char[] hexArray = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+    public static String bytesToHexString(byte[] bytes) {
+        char[] hexChars = new char[bytes.length*2];
+        int v;
+
+        for(int j=0; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j*2] = hexArray[v>>>4];
+            hexChars[j*2 + 1] = hexArray[v & 0x0F];
+        }
+
+        return new String(hexChars);
+    }
+
 
     public static String[] getStringsFromUUIDs(UUID[] uuids) {
         String[] params = new String[uuids.length];
