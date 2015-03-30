6bf64ff76b6fe4f47dded848fbebc5d90e3b7b39
Tijl <tijl@houtbecke.rs>
2015-03-30 15:35:49 +0200

    clarify comment

--- LeBlue/src/main/java/houtbecke/rs/le/fourdotthree/LeRemoteDevice43.java
+++ LeBlue/src/main/java/houtbecke/rs/le/fourdotthree/LeRemoteDevice43.java
@@ -91,7 +91,8 @@ public class LeRemoteDevice43 extends BluetoothGattCallback implements LeRemoteD
     }
     @Override
     public void startServicesDiscovery(UUID... uuids){
-        //scanning for specific services is not supported on on android
+        //scanning for specific services is not supported by the default implementation, 
+        // because scanning for specific services can be very buggy, eg: https://code.google.com/p/android/issues/detail?id=59490
         this.startServicesDiscovery();
     }
     @Override
