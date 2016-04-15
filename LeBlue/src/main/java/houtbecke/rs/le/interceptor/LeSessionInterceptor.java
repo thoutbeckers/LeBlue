0417cea7288b699e5d75f6b21a5006fc7714feba
Jasper van Putten <jasper@splendo.com>
2016-04-15 13:14:02 +0200

    prevent null pointer exception

--- LeBlue/src/main/java/houtbecke/rs/le/interceptor/LeSessionInterceptor.java
+++ LeBlue/src/main/java/houtbecke/rs/le/interceptor/LeSessionInterceptor.java
@@ -221,13 +221,16 @@ public class LeSessionInterceptor extends LeInterceptor {
     @Override
     public void characteristicListenerSet(InterceptingLeRemoteDevice iLeRemoteDevice, InterceptingLeCharacteristicListener iCharacteristicsListener, UUID[] uuids) {
         String[] args=null;
+        String characteristicsListenerId = "";
+        if (iCharacteristicsListener!=null)
+            characteristicsListenerId = iCharacteristicsListener.id+"";
         if  (uuids != null){
             args = new String[1 + uuids.length];
-            args[0] = iCharacteristicsListener.id+"";
+            args[0] = characteristicsListenerId;
             LeUtil.putUUIDsInStringArray(uuids, args, 1);
         } else{
             args = new String[1];
-            args[0] = iCharacteristicsListener.id+"";
+            args[0] = characteristicsListenerId;
         }
 
         drainEvent(remoteDeviceSetCharacteristicListener, iLeRemoteDevice, args);
