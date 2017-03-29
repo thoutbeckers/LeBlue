a0e8e103669bbea1d78da66c17c4a534c245f5e5
Jasper van Putten <jasper@splendo.com>
2017-03-29 16:20:49 +0200

    prevent concurrency error

--- LeBlue-java/src/main/java/houtbecke/rs/le/session/ListEventSinkSource.java
+++ LeBlue-java/src/main/java/houtbecke/rs/le/session/ListEventSinkSource.java
@@ -2,10 +2,14 @@ package houtbecke.rs.le.session;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ListEventSinkSource implements EventSink, EventSource {
 
     LinkedList<Event> events = new LinkedList<>();
+    ReadWriteLock lock = new ReentrantReadWriteLock();
+
     Iterator<Event> iterator = null;
     int limit = -1;
 
@@ -20,45 +24,68 @@ public class ListEventSinkSource implements EventSink, EventSource {
 
     @Override
     public void addEvent(Event event) {
-        if (limit == -1){
-            events.addLast(event);
-        }else{
-            if (limit <= events.size())
-                events.removeFirst();
-            events.addLast(event);
+        lock.writeLock().lock();
+        try {
+            if (limit == -1) {
+                events.addLast(event);
+            } else {
+                if (limit <= events.size())
+                    events.removeFirst();
+                events.addLast(event);
+            }
+        } finally {
+            lock.writeLock().unlock();
+
         }
 
     }
-
-
     private void correctDelay(){
-        long lastTimeStamp =0;
-        for (Event event : events) {
-            if (lastTimeStamp == 0) {
-                event.delay = 0;
-            } else {
-                event.delay = (int) (event.timeStamp - lastTimeStamp);
+        lock.writeLock().lock();
+        try {
+            long lastTimeStamp = 0;
+            for (Event event : events) {
+                if (lastTimeStamp == 0) {
+                    event.delay = 0;
+                } else {
+                    event.delay = (int) (event.timeStamp - lastTimeStamp);
+                }
+                lastTimeStamp = event.timeStamp;
             }
-            lastTimeStamp = event.timeStamp;
+        }finally {
+            lock.writeLock().unlock();
+
         }
+
     }
 
     @Override
     public Event[] getEvents(){
-        this.correctDelay();
-        return events.toArray(new Event[events.size()]
+        lock.readLock().lock();
+        try {
+            this.correctDelay();
+            return events.toArray(new Event[events.size()]
 
 
-        );
+            );
+        }finally {
+            lock.readLock().unlock();
+
+        }
     }
 
 
     @Override
     public Event nextEvent() {
-        if (iterator == null)
-            iterator = events.iterator();
+        lock.readLock().lock();
+        try {
+            if (iterator == null)
+                iterator = events.iterator();
 
-        return iterator.next();
+            return iterator.next();
+        }finally {
+            lock.readLock().unlock();
+
+        }
     }
 
     @Override
