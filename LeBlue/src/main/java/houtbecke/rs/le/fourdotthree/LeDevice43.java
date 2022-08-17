5d6ed6f56a01717faadb6b38c05352515806802a
Jasper van Putten <jasper@tacx.com>
2022-08-17 12:11:20 +0200

    add android 12 support

--- LeBlue/src/main/java/houtbecke/rs/le/fourdotthree/LeDevice43.java
+++ LeBlue/src/main/java/houtbecke/rs/le/fourdotthree/LeDevice43.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Handler;
 import android.os.ParcelUuid;
 import android.util.Log;
@@ -47,46 +48,133 @@ import no.nordicsemi.android.support.v18.scanner.ScanSettings;
 
 public class LeDevice43 implements LeDevice {
 
-    final Context context;
-
-    public void setErrorLogger(@NonNull ErrorLogger errorLogger) {
-        if (errorLogger != null) { this.errorLogger = errorLogger; }
-    }
-
-    protected void log(int priority, String tag, String msg){
-        if (errorLogger!=null){
-            errorLogger.log(priority,tag,msg);
-        }else{
-            Log.println(priority,tag,msg);
-        }
-    }
-
-    protected void logException(Exception e){
-        if (errorLogger!=null){
-            errorLogger.logException(e);
-        }else{
-            if (e!=null)
-                e.printStackTrace();
-        }
-    }
-
-
-    ErrorLogger errorLogger;
-
-    Collection<LeDeviceListener> listeners = new HashSet<>();
-    private final ReadWriteLock listenerReadWriteLock = new ReentrantReadWriteLock();
-
     private interface L {
         void l(LeDeviceListener l);
     }
 
-    private void listeners(L l) {
-        listenerReadWriteLock.readLock().lock();
+    final Context context;
+    final BluetoothManager mBluetoothManager;
+    final BluetoothAdapter bluetoothAdapter;
+    private final ReadWriteLock listenerReadWriteLock = new ReentrantReadWriteLock();
+    private final Handler mTimerHandler = new Handler();
+    protected Map<String, LeRemoteDevice43> remoteDevices = new LinkedHashMap<String, LeRemoteDevice43>();
+    ErrorLogger errorLogger;
+    Collection<LeDeviceListener> listeners = new HashSet<>();
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
+                        BluetoothAdapter.ERROR);
+                LeDeviceState deviceState;
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        deviceState = LeDeviceState.OFF;
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        deviceState = LeDeviceState.ON;
+                        break;
+                    default:
+                        return;
+                }
+
+                final LeDeviceState finalLeDeviceState = deviceState;
+                listeners(
+                        l -> l.leDeviceState(LeDevice43.this, finalLeDeviceState));
+
+            } else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+
+                final LeRemoteDevice43 remoteDevice43 = remoteDevices.remove(device.getAddress());
+                if (remoteDevice43 != null) {remoteDevice43.notifyDisconnected();}
+            }
+        }
+    };
+    /* defines callback for scanning results */
+    private final BluetoothAdapter.LeScanCallback deviceFoundCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
+            final LeRemoteDevice43 device43 = new LeRemoteDevice43(LeDevice43.this, device);
+
+            listeners(
+                    l -> l.leDeviceFound(LeDevice43.this, device43, rssi, LeUtil.parseLeScanRecord(scanRecord)));
+
+        }
+    };
+    /* defines callback for scanning results */
+    private final ScanCallback scanCallback = new ScanCallback() {
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult result : results) {
+                sendScanResult(result);
+
+            }
+        }
+
+        public void onScanFailed(int errorCode) {
+            log(Log.ERROR, "LeBlue", "scan record: " + "onScanFailed: " + errorCode);
+        }
+
+        public void onScanResult(int callbackType, ScanResult result) {
+            if (callbackType == CALLBACK_TYPE_MATCH_LOST) {return;}
+            sendScanResult(result);
+        }
+
+        void sendScanResult(final ScanResult result) {
+            final LeRemoteDevice43 device43 = new LeRemoteDevice43(LeDevice43.this, result.getDevice());
+            listeners(
+                    l -> l.leDeviceFound(LeDevice43.this, device43, result.getRssi(), LeUtil.parseLeScanRecord(result.getScanRecord().getBytes())));
+
+        }
+
+    };
+
+    @Inject
+    public LeDevice43(Context context) throws BleException {
+        this.context = context;
         try {
-            for (LeDeviceListener listener: listeners)
-                l.l(listener);
-        } finally {
-            listenerReadWriteLock.readLock().unlock();
+            mBluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+
+            if (mBluetoothManager == null) {
+                throw new BleException("Bluetooth Manager not found");
+            }
+
+            bluetoothAdapter = mBluetoothManager.getAdapter();
+            if (bluetoothAdapter == null) {
+                throw new BleException("Bluetooth Adapter not found");
+            }
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
+            filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
+            filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
+            context.registerReceiver(receiver, filter);
+
+        } catch (BleException ble) {
+            throw ble;
+        } catch (Exception e) {
+            throw new BleException("Error initializing Bluetooth adapter", e);
+        }
+    }
+
+    public void setErrorLogger(@NonNull ErrorLogger errorLogger) {
+        if (errorLogger != null) {this.errorLogger = errorLogger;}
+    }
+
+    protected void log(int priority, String tag, String msg) {
+        if (errorLogger != null) {
+            errorLogger.log(priority, tag, msg);
+        } else {
+            Log.println(priority, tag, msg);
+        }
+    }
+
+    protected void logException(Exception e) {
+        if (errorLogger != null) {
+            errorLogger.logException(e);
+        } else {
+            if (e != null) {e.printStackTrace();}
         }
     }
 
@@ -110,142 +198,38 @@ public class LeDevice43 implements LeDevice {
         }
     }
 
-    @Inject
-    public LeDevice43(Context context) throws BleException {
-        this.context = context;
-        try {
-            mBluetoothManager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
-
-            if (mBluetoothManager == null) {
-                throw new BleException("Bluetooth Manager not found");
-            }
-
-            bluetoothAdapter = mBluetoothManager.getAdapter();
-            if (bluetoothAdapter == null) {
-                throw new BleException("Bluetooth Adapter not found");
-            }
-            IntentFilter filter = new IntentFilter();
-            filter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-            filter.addAction(BluetoothDevice.ACTION_ACL_CONNECTED);
-            filter.addAction(BluetoothDevice.ACTION_ACL_DISCONNECTED);
-            context.registerReceiver(receiver, filter);
-
-        } catch (BleException ble) {
-            throw  ble;
-        } catch (Exception e) {
-            throw new BleException("Error initializing Bluetooth adapter", e);
-        }
-    }
-
-    private final BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
-                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE,
-                        BluetoothAdapter.ERROR);
-                LeDeviceState deviceState;
-                switch (state){
-                    case BluetoothAdapter.STATE_OFF:
-                        deviceState = LeDeviceState.OFF;
-                        break;
-                    case BluetoothAdapter.STATE_ON:
-                        deviceState = LeDeviceState.ON;
-                        break;
-                    default:
-                        return;
-                }
-
-
-                final LeDeviceState finalLeDeviceState = deviceState;
-                listeners(
-                        l -> l.leDeviceState(LeDevice43.this, finalLeDeviceState));
-
-            }  else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
-            } else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
-
-                final LeRemoteDevice43 remoteDevice43 = remoteDevices.remove(device.getAddress());
-                if (remoteDevice43 != null)
-                    remoteDevice43.notifyDisconnected();
-            }
-        }
-    };
-
-
     @Override
     public boolean checkBleHardwareAvailable() {
-		// First check general Bluetooth Hardware:
-		// get BluetoothManager...
-		final BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
-		if(manager == null) return false;
-		// .. and then get adapter from manager
-		final BluetoothAdapter adapter = manager.getAdapter();
-		if(adapter == null) return false;
-		
-		// and then check if BT LE is also available
+        // First check general Bluetooth Hardware:
+        // get BluetoothManager...
+        final BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        if (manager == null) {return false;}
+        // .. and then get adapter from manager
+        final BluetoothAdapter adapter = manager.getAdapter();
+        if (adapter == null) {return false;}
+
+        // and then check if BT LE is also available
         return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE);
-	}    
+    }
 
-	
-	@Override
+    @Override
     public boolean isBtEnabled() {
-        if  (ContextCompat.checkSelfPermission(context,Manifest.permission.BLUETOOTH) != PERMISSION_GRANTED)
+        if (!hasPermission()) {
             return false;
-		final BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
-		if(manager == null) return false;
-		
-		final BluetoothAdapter adapter = manager.getAdapter();
-		if(adapter == null) return false;
-		
-		return adapter.isEnabled();
-	}
-
-    /* defines callback for scanning results */
-    private final BluetoothAdapter.LeScanCallback deviceFoundCallback = new BluetoothAdapter.LeScanCallback() {
-        @Override
-        public void onLeScan(final BluetoothDevice device, final int rssi, final byte[] scanRecord) {
-            final LeRemoteDevice43 device43 = new LeRemoteDevice43(LeDevice43.this, device);
-
-            listeners(
-                    l -> l.leDeviceFound(LeDevice43.this, device43, rssi, LeUtil.parseLeScanRecord(scanRecord)));
-
-        }
-    };
-
-    /* defines callback for scanning results */
-    private final ScanCallback scanCallback = new ScanCallback() {
-        public void onBatchScanResults(List<ScanResult> results) {
-            for (ScanResult result : results) {
-                sendScanResult(result);
-
-            }
         }
 
-        public void onScanFailed(int errorCode) {
-            log(Log.ERROR, "LeBlue", "scan record: " + "onScanFailed: " + errorCode);
-        }
+        final BluetoothManager manager = (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        if (manager == null) {return false;}
 
-        public void onScanResult(int callbackType, ScanResult result) {
-            if (callbackType==CALLBACK_TYPE_MATCH_LOST) return;
-                sendScanResult(result);
-            }
+        final BluetoothAdapter adapter = manager.getAdapter();
+        if (adapter == null) {return false;}
 
-        void sendScanResult(final ScanResult result){
-            final LeRemoteDevice43 device43 = new LeRemoteDevice43(LeDevice43.this, result.getDevice());
-            listeners(
-                    l -> l.leDeviceFound(LeDevice43.this, device43, result.getRssi(), LeUtil.parseLeScanRecord(result.getScanRecord().getBytes())));
-
-        }
-
-    };
-
-
+        return adapter.isEnabled();
+    }
 
     @Override
     public void startScanning() {
-        if(!hasPermission() )return;
+        if (!hasPermission()) {return;}
         BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
         scanner.startScan(scanCallback);
     }
@@ -280,20 +264,17 @@ public class LeDevice43 implements LeDevice {
         startScanning(scanner, scanFilters);
     }
 
-    private final Handler mTimerHandler = new Handler();
-
     @Override
-	public void stopScanning() {
-        if(!hasPermission() )return;
+    public void stopScanning() {
+        if (!hasPermission()) {return;}
         BluetoothLeScannerCompat scanner = BluetoothLeScannerCompat.getScanner();
         scanner.stopScan(scanCallback);
-	}
-
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || !(o instanceof LeDevice43)) return false;
+        if (this == o) {return true;}
+        if (o == null || !(o instanceof LeDevice43)) {return false;}
         LeDevice43 that = (LeDevice43) o;
         return that.bluetoothAdapter.getAddress().equals(bluetoothAdapter.getAddress());
 
@@ -302,21 +283,6 @@ public class LeDevice43 implements LeDevice {
     @Override
     public int hashCode() {
         return bluetoothAdapter.getAddress().hashCode();
-    }
-
-    protected Map<String, LeRemoteDevice43> remoteDevices = new LinkedHashMap<String, LeRemoteDevice43>();
-
-    final BluetoothManager mBluetoothManager;
-    final BluetoothAdapter bluetoothAdapter;
-
-    private void startScanning(final BluetoothLeScannerCompat scanner, final List<ScanFilter> scanFilters) {
-        final ScanSettings settings = new ScanSettings.Builder()
-                .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
-                .setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT)
-                .setUseHardwareBatchingIfSupported(false)
-                .setUseHardwareFilteringIfSupported(false)//scanning with filters is unreliable on some older samsung phones
-                .build();
-        scanner.startScan(scanFilters, settings, scanCallback);
     }
 
     LeGattStatus toGattStatus(int status) {
@@ -343,23 +309,44 @@ public class LeDevice43 implements LeDevice {
         }
     }
 
-
     public void disable() {
         bluetoothAdapter.disable();
     }
-  
+
     public void enable() {
         bluetoothAdapter.enable();
     }
 
-    private boolean hasPermission(){
-        boolean bluetooth = ContextCompat.checkSelfPermission(context,
-                Manifest.permission.BLUETOOTH_ADMIN) ==PERMISSION_GRANTED;
-        boolean coarse = ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_COARSE_LOCATION) ==PERMISSION_GRANTED;
-        boolean fine = ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION) ==PERMISSION_GRANTED;
-        return (bluetooth && (coarse || fine));
+    private void listeners(L l) {
+        listenerReadWriteLock.readLock().lock();
+        try {
+            for (LeDeviceListener listener : listeners)
+                l.l(listener);
+        } finally {
+            listenerReadWriteLock.readLock().unlock();
+        }
+    }
 
+    private void startScanning(final BluetoothLeScannerCompat scanner, final List<ScanFilter> scanFilters) {
+        final ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_BALANCED)
+                .setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT)
+                .setUseHardwareBatchingIfSupported(false)
+                .setUseHardwareFilteringIfSupported(false)//scanning with filters is unreliable on some older samsung phones
+                .build();
+        scanner.startScan(scanFilters, settings, scanCallback);
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            return ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.BLUETOOTH_CONNECT) == PERMISSION_GRANTED;
+        } else {
+            boolean bluetooth_admin = ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.BLUETOOTH_ADMIN) == PERMISSION_GRANTED;
+            boolean fine = ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+            return bluetooth_admin && fine;
+        }
     }
 }
